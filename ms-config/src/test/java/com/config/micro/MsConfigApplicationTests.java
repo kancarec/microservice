52ce6431dda31c059e766ce09fdf344f06564d06
kancarec <recepkanca11@gmail.com>
2021-05-02 21:18:31 +0300

    mvn

---
+++ ms-config/src/test/java/com/config/micro/MsConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.config.micro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
